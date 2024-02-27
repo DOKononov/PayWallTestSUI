0dbc2e935206760ec87ef66a3f1487bb79357639
d.o.kononov <d.o.kononov@gmail.com>
2024-02-27 18:27:33 +0300

    Initial Commit

---
+++ PayWallTestSUI/PayWallTestSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PayWallTestSUIApp.swift
+//  PayWallTestSUI
+//
+//  Created by Dmitry Kononov on 27.02.24.
+//
+
+import SwiftUI
+
+@main
+struct PayWallTestSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
